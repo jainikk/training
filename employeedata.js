8cdecc44b8d379e2116355449e71139529ddfbce
jainikk <jainikk@cybage.com>
2016-09-06 11:48:15 +0530

    Delete employeedata.js

--- employeedata.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('employeesCtrl', function($scope, $http) {
-    $http.get("package.json").then(function(response) {
-        $scope.myData = response.data.employee;
-		
-    });
-	
-});
